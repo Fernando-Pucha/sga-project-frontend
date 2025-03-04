fe7dcb23d96bd70de355245fe3f5983dd95aa48e
Fernando-Pucha <fernandopuchap@gmail.com>
2025-03-04 21:10:09 +0000

    done start frontend, install tailwind and daisy ui

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,12 @@
+import "./HomePage.css";
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Home page</h1>
+      <button className="btn btn-outline btn-success">Success</button>
+    </div>
+  );
+}
+
+export default HomePage;
